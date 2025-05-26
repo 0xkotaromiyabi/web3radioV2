a91b46fd2be3d29af990f18a3103b4200e9f57d1
Kotarominami <36185214+wahyudieko87@users.noreply.github.com>
2025-05-26 15:20:23 +0800

    farcaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-
+import { useEffect } from "react";
+import { sdk } from "@farcaster/frame-sdk";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import News from "./pages/News";
 import Events from "./pages/Events";
@@ -12,22 +14,26 @@ import CMS from "./pages/CMS";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/stations" element={<Stations />} />
-          <Route path="/cms" element={<CMS />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+export default function App() {
+  useEffect(() => {
+    sdk.actions.ready({ disableNativeGestures: true });
+  }, []);
 
-export default App;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/stations" element={<Stations />} />
+            <Route path="/cms" element={<CMS />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
